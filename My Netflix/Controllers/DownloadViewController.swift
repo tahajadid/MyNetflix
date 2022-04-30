ebbfedd77a2ddee4c3294ecdd9b436e381d23951
taha <taha.jadid>
2022-04-30 21:43:52 +0000

    First Commit

---
+++ My Netflix/Controllers/DownloadViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DownloadViewController.swift
+//  My Netflix
+//
+//  Created by taha_jadid on 29/4/2022.
+//
+
+import UIKit
+
+class DownloadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+
+    }
+    
+
+}
--- My Netflix/Controllers/UpcomingViewController.swift
+++ My Netflix/Controllers/UpcomingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  UpcomingViewController.swift
 //  My Netflix
 //
 //  Created by taha_jadid on 29/4/2022.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class UpcomingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        view.backgroundColor = .systemBackground
+
+    }
+    
 
 }
-
