6840d9fa4b838dc65fc5e5cd3222f723b53c6714
taha <taha.jadid>
2022-05-07 12:18:21 +0000

    Add Header

--- My Netflix/Controllers/HomeViewController.swift
+++ My Netflix/Controllers/HomeViewController.swift
@@ -11,8 +11,8 @@ class HomeViewController: UIViewController {
 
     private let homeFeedTable: UITableView = {
        
-        let table = UITableView()
-        table.register(UITableViewCell.self , forCellReuseIdentifier: "cell")
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(CollectionViewTableViewCell.self , forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
         return table
     }()
     
@@ -24,6 +24,9 @@ class HomeViewController: UIViewController {
         
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
+        
+        let heroHeaderView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 480))
+        homeFeedTable.tableHeaderView = heroHeaderView
 
     }
     
@@ -36,14 +39,26 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 20
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Hello world!"
-        return cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
         
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
     }
 }
